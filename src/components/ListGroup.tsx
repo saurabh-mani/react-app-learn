4e69ccffa7aa50c6893c46b17fa97f49dd3fc9a9
saurabh-mani <saurabhmani00@gmail.com>
2024-08-26 23:41:20 +0530

    ** Dynamic ListGroups ** Multiple Tags under one component enclosed within <> and </>

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,16 @@
 function ListGroup() {
+  const items = ["Java", "C++", "C", "C#", "PHP", "ASP.NET"];
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item} className="list-group-item">
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
