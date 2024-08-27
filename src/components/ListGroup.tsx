68efd0e903cb495d9a25d96d5eba4c64754df5c7
saurabh-mani <saurabhmani00@gmail.com>
2024-08-27 12:42:15 +0530

    ** Conditional Rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,8 +1,13 @@
 function ListGroup() {
-  const items = ["Java", "C++", "C", "C#", "PHP", "ASP.NET"];
+  var items = ["Java", "C++", "C", "C#", "PHP", "ASP.NET"];
+  items = [];
+  var getMessage = () => {
+    return items.length == 0 && <p>No Items Found...</p>;
+  };
   return (
     <>
       <h1>List</h1>
+      {getMessage()}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item} className="list-group-item">
