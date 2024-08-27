624ea420d803650095970dc1751938325122f4aa
saurabh-mani <saurabhmani00@gmail.com>
2024-08-27 12:59:39 +0530

    ** Event Handling

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,21 @@
+import React from "react";
+import { MouseEvent } from "react";
+
 function ListGroup() {
   var items = ["Java", "C++", "C", "C#", "PHP", "ASP.NET"];
-  items = [];
-  var getMessage = () => {
-    return items.length == 0 && <p>No Items Found...</p>;
+  //items = [];
+
+  //Event Handler
+  const handleClick = (event: MouseEvent) => {
+    console.log(event);
   };
   return (
     <>
       <h1>List</h1>
-      {getMessage()}
+      {items.length == 0 && <p>No Items Found...</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item} className="list-group-item">
+        {items.map((item, index) => (
+          <li key={item} className="list-group-item" onClick={handleClick}>
             {item}
           </li>
         ))}
